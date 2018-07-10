480ad5e6bd1dd2e491e4a43c54da150ecddd546c
HuaBa <liuhualin333@gmail.com>
2018-07-10 15:34:44 +0800

    Add vue rendering, adjust mobile look

--- js/index.js
+++ js/index.js
@@ -1,6 +1,35 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hualin'
-  }
+// Brand List Component
+Vue.component('contact-item', {
+	props: ['contact'],
+	template: `<li class="pr-4">
+				<a :href="contact.link">
+					<span :class="contact.afclass" style="font-size:36px;color:white"></span>
+				</a>
+			</li>`
+})
+// NavBar List Component
+Vue.component('navbar-item', {
+	props: ['item'],
+	template: `<li class="nav-item"><a class="nav-link" :href="item.link">{{ item.name }}</a></li>`
+})
+
+var footerApp = new Vue({
+	el: '#footerApp',
+	data: {
+		brandList: [
+			{ id: 0, link: 'https://github.com/liuhualin333/', afclass:"fab fa-github" },
+			{ id: 1, link: 'https://www.linkedin.com/in/visionaryliu/', afclass:"fab fa-linkedin" },
+			{ id: 2, link: 'mailto:liuhualin333@gmail.com', afclass:"fas fa-envelope"}
+		]
+	}
+})
+var contactApp = new Vue({
+	el: '#navbarItemApp',
+	data: {
+		name: "Hualin Liu",
+		itemList: [
+			{ id: 0, link: './cv.html', name:"Curriculum Vitae" },
+			{ id: 1, link: './blog.html', name:"Articles" }
+		]
+	}
 })
