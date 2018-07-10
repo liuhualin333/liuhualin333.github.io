00b22a5b692ef014dafe5e35daaf95b91fd8910f
HuaBa <liuhualin333@gmail.com>
2018-07-10 18:37:08 +0800

    Refine layout

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-// Brand List Component
+// Contact List Component
+Vue.component('contact-block', {
+	props: ['type'],
+	template: `<div :class="type.class">
+					<h4 class="text-white">Contact Me</h4>
+					<ul :class="type.ulclass">
+						<slot></slot>
+					</ul>
+				</div>`
+})
 Vue.component('contact-item', {
 	props: ['contact'],
-	template: `<li class="pr-4">
+	template: `<li class="pr-md-4 pr-lg-4">
 				<a :href="contact.link">
 					<span :class="contact.afclass" style="font-size:36px;color:white"></span>
 				</a>
@@ -12,10 +21,11 @@ Vue.component('navbar-item', {
 	props: ['item'],
 	template: `<li class="nav-item"><a class="nav-link" :href="item.link">{{ item.name }}</a></li>`
 })
-
 var footerApp = new Vue({
 	el: '#footerApp',
 	data: {
+		largeContact:{class:"col-6 col-sm-4 offset-md-1 py-4 d-none d-md-block", ulclass:"list-unstyled d-flex py-2"},
+		smallContact:{class:"col-6 col-sm-4 offset-md-1 py-4 d-block d-md-none mx-auto text-center", ulclass:"list-unstyled d-flex px-2 py-2 mx-auto justify-content-between"},
 		brandList: [
 			{ id: 0, link: 'https://github.com/liuhualin333/', afclass:"fab fa-github" },
 			{ id: 1, link: 'https://www.linkedin.com/in/visionaryliu/', afclass:"fab fa-linkedin" },
@@ -33,3 +43,9 @@ var contactApp = new Vue({
 		]
 	}
 })
+function resizeImage() {
+    //calculations here...
+
+}
+
+window.addEventListener('resize', resizeImage, false);
